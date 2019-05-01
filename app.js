f65360fe53457bb2132ce740e7469a59333a0f1c
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-05-01 11:15:12 -0300

    Configurando a pasta public no arquivo app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
   const bodyParser = require ('body-parser');
   const app = express();
   const admin = require('./routers/admin')
+  const path = require ('path')
   //const mongoose = require ('mongoose');
 
 // Configurações
@@ -15,6 +16,8 @@
       app.set('view engine', 'handlebars')
   // Mongoose
       // Em breve
+  // Public
+      app.use(express.static(path.join(__dirname, "public")))
 // Rotas
     app.use('/admin', admin)
 // Outros
