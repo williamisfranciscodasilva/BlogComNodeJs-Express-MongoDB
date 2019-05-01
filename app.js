8dd41757e8aeae144f7903c29dbecd5eca544648
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-05-01 09:00:30 -0300

    Criação do arquivo principal app.js, contendo a estrutura que usaremos já com as constantes dos módulos declaradas e chamando a porta 8081

---
+++ app.js
@@ -0,0 +1,16 @@
+// Carregando módulos
+  const express = require ('express');
+  const handlebars = require ('express-handlebars');
+  const bodyParser = require ('body-parser');
+  const app = express();
+  //const mongoose = require ('mongoose');
+
+// Configurações
+
+// Rotas
+
+// Outros
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log("Servidor Rodando!")
+})
